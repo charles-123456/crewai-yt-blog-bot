1107d461eecdc8b1a92c32cdf95f6e6550fab312
Charles <charlesit333@gmail.com>
2025-04-24 14:22:44 +0530

    module added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # streamlit_app.py
-#__import__('pysqlite3')
-#import sys
-#sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import streamlit as st
 from crewai import Crew, Process
